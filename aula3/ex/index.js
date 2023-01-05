6317513c2f56d2dc68fd669fe04e372cfd439901
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-05 18:26:43 -0300

    aula3 - console.log

---
+++ aula3/ex/index.js
@@ -0,0 +1,1 @@
+console.log(`Meu nome é "Felipe". Estou aprendendo JavaScript às `, 18,`da tarde.`)
---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Hello, "world!"'); // Método .log dentro do objeto console
+console.log("Hello, 'world'!"); // String = Texto
+console.log(`'Hello', "world"!`);
+
+console.log(123456, 15.85, 'Hello, world! again'); // Number = Número
