89088c2ff48ebff77bc02058d0d0ed7309cf3bbd
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-06 00:32:34 -0300

    aula 4 e aula 5

---
+++ aula4/comentarios.js
@@ -0,0 +1,11 @@
+// Isso é um comentário
+console.log('Hello, world!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// comentário
+
+/* 
+  Um comentário
+  de várias linhas
+*/
+console.log('Linha 2');
+// Vai ser ignorado
---
+++ aula5/index.js
@@ -0,0 +1,3 @@
+// comentário
+console.log('123')
+console.log('Este trecho será exibido no console do navegador.')
