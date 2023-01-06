ac617b201ae7a8bbaeede0f14b467a83c3fd6afe
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-06 15:45:25 -0300

    Aula 6
    -let;

---
+++ aula6/index.js
@@ -0,0 +1,20 @@
+let nome = 'Henrique'
+
+console.log(nome, 'nasceu em 1984.')
+console.log('Em 2000', nome,'conheceu Maria.')
+console.log(nome,'casou-se com Maria em 2012.')
+console.log('Maria teve 1 filho com', nome,'em 2015.')
+console.log('O filho de', nome, 'se chama Eduardo.')
+
+let investimento; // Declarou a variável
+investimento = 'R$100.000,00' // Inicializando a variável
+
+
+// Não podemos criar variáveis com palavras reservadas;
+// Variáveis precisam ter nomes significativos;
+// Não podem começar o nome de uma variável com um número;
+// Não podem conter espaços ou traços;
+// Utilizamos camelCase;
+// Variáveis são case-sensitive;
+// Não podemos redeclarar variáveis com let;
+// NÃO UTILIZE VAR, UTILIZE LET.
