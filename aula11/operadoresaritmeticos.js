da4f06d8b1ea8a4bcb25946485cc51885f6d8ba7
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-06 23:01:01 -0300

    aula 11 - Operadores Aritméticos

---
+++ aula11/operadoresaritmeticos.js
@@ -0,0 +1,27 @@
+/*
+  Aritméticos:
+  + Adição ou Concatenação
+  - Subtração
+  * Multiplicação
+  / Divisão
+  ** Potenciação
+  % Resto da divisão
+
+  Incremento = ++
+  Decremento = --
+
+Ordem de precedencia:
+()
+**
+* / %
++-
+
+*/
+
+// NaN - Not a number
+const num1 = 10;
+const num2 = parseInt('5')
+console.log(num1 + num2)
+
+// Transformando strings para número:
+// parseInt(inteiro), parseFloat(decimais), Number('5')
