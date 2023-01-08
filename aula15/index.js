09c0fac5732bb4430e29d6660b223db4eebfba42
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-08 13:16:14 -0300

    aula15 - Math

---
+++ aula15/index.js
@@ -0,0 +1,14 @@
+let num1 = 9
+console.log(100/0) // O retorno será 'infinity', e retorna true se perguntar se a conta deu certo
+
+
+
+//console.log(num1 ** (1/2)) // Raiz quadrada
+// let num2 = Math.floor(num1) (Arredondamento para baixo)
+// let num2 = Math.ceil(num1) (Arredondamento para cima)
+// let num2 = Math.round(num1) (Arredondamento para o número inteiro mais próximo)
+
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)) - O maior da sequência.
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)) - O menor da sequência.
+
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5) - Gerando valor aleatório entre 5 e 10.
