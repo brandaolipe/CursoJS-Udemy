5df6e505d57992524ac220240665157ba159205b
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-08 18:42:36 -0300

    aula 16 - Arrays (Básico)

---
+++ aula16/index.js
@@ -0,0 +1,22 @@
+// index          0        1       2 
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+
+console.log(typeof alunos) // retorna object
+console.log(alunos instanceof Array) // Se for array retorna true;
+
+//alunos.push('Bruna')
+//alunos.push('Lucas')
+
+//console.log(alunos.slice(0, 3)) // inicio e final não incluso
+
+//delete alunos[1] // Deleta sem mudar o índice. Retorna undefined
+
+//const removido = alunos.shift() // .shift() remove do inicio do array, o que foi removido pode ser guardado em uma variável.
+
+//const removido = alunos.pop() // .pop() remove do final do array, o que foi removido pode ser guardado em uma variável.
+
+// alunos.unshift('Luiza') // Adiciona no começo do array
+
+// alunos.push('Luiza') // Adiciona no final do array
