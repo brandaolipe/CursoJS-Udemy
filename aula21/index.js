04f2a71401d9509fb5e72657d6a33e74d405aba8
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-10 11:14:18 -0300

    Aula 21 - Operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação
+>
+>=
+<
+<=
+== - Não recomendado
+===
+!= - Não recomendado
+!==
+*/
+const num1 = 10;
+const num2 = 10;
+const comp = num1 === num2;
+console.log(comp);
+
