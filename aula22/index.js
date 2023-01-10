055be8f84a570687f772608c42381c0605aac819
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-10 11:41:38 -0300

    Aula 22 - Operadores Lógicos

---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores Lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+const expressaoAnd = true && true && false && true
+const expressaoOr = false || false || true || false
+console.log(expressaoOr)
+
+console.log(!true)
