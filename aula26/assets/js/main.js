9d8d77239ab3b7e56b0ae4653c8206b2cd4620a3
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-12 22:18:12 -0300

    aula 26 - exercício do IMC

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -20,8 +20,11 @@ form.addEventListener('submit', function (e) {
     }
 
     const imc = getImc(peso, altura)
+    const nivelImc = getNivelImc(imc)
 
-    console.log(imc)
+    const msg = `Seu IMC é ${imc} (${nivelImc})`
+
+    setResultado(msg, true)
     
 })
 
@@ -35,6 +38,13 @@ function setResultado (msg, isValid) {
     resultado.innerHTML = ''
     
     const p = criaP()
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado')   
+    } else {
+        p.classList.add('bad')
+    }
+
     p.innerHTML = msg;
     resultado.appendChild(p)
 }
@@ -42,4 +52,15 @@ function setResultado (msg, isValid) {
 function getImc (peso, altura) {
     const imc = peso / altura ** 2
     return imc.toFixed(2)
+}
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+    if (imc >= 39.9) return nivel[5]
+    if (imc >= 34.9) return nivel[4]
+    if (imc >= 29.9) return nivel[3]
+    if (imc >= 24.9) return nivel[2]
+    if (imc >= 18.5) return nivel[1]
+    if (imc < 18.5) return nivel[0]
 }
