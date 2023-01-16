ee0c63105f6e5d6d0bad8b04083f213b8057512f
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-16 19:05:35 -0300

    Aula 34 - For (estrutura de repetição clássica)

---
+++ aula34/index.js
@@ -0,0 +1,10 @@
+// for ( let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0?'par':'ímpar'
+//     console.log(i, par)
+// } // Não tem ponto e virgula nem opcional.
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Melancia', 'Pêssego']
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i])
+}
