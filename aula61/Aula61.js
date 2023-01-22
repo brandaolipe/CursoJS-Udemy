ce1b986bb1cb80888bf0e5ddf38b0dc5868164d0
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-22 20:42:21 -0300

    Aula 61 - Funções geradoras

---
+++ aula61/Aula61.js
@@ -0,0 +1,19 @@
+function* geradora1() {
+    // Código qualquer ...
+    yield 'Valor 1'
+    // Código qualquer ...
+    yield 'Valor 2'
+    // Código qualquer ...
+    yield 'Valor 3'
+}
+
+const g1 = geradora1()
+
+// console.log(g1.next().value)
+// console.log(g1.next().value)
+// console.log(g1.next())
+// console.log(g1.next())
+
+for (let valor of g1){
+    console.log(valor)
+}
