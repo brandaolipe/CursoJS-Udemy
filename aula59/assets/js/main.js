b45fad5e8b50f07f3d62a61d672fca9b7d88c96c
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-22 19:57:08 -0300

    Aula 59 - Calculadora com função construtora

---
+++ aula59/assets/js/main.js
@@ -0,0 +1,55 @@
+function Calculadora() {
+    this.display = document.querySelector('.display')
+
+    this.inicia = () => {
+        this.cliqueBotoes()
+        this.pressionaEnter()
+    }
+
+    this.clearDisplay = () => this.display.value = ''
+
+    this.deleteOne = () => this.display.value = this.display.value.slice(0, -1)
+        
+    this.pressionaEnter = () => {
+        this.display.addEventListener('keyup', e => {
+            if (e.keyCode === 13) {
+                this.realizaConta()
+            }
+        })
+    }
+
+    this.realizaConta = () => {
+        let conta = this.display.value
+        
+        try {
+            conta = eval(conta)
+        
+            if(!conta) {
+                alert('Conta inválida')
+                return
+            }
+        
+            this.display.value = String(conta)
+        } catch(e) {
+            alert('Conta inválida!')
+            return
+        }
+    }   
+         
+    this.cliqueBotoes = () => {
+        document.addEventListener('click', (e) =>{
+            const el = e.target                    
+            if (el.classList.contains('btn-num')) this.btnParaDisplay(el.innerText)
+            if (el.classList.contains('btn-clear')) this.clearDisplay()
+            if (el.classList.contains('btn-del')) this.deleteOne()
+            if (el.classList.contains('btn-eq')) this.realizaConta()      
+            this.display.focus()
+        })
+    }
+    
+    this.btnParaDisplay = valor => this.display.value += valor
+}
+
+
+const calculadora = new Calculadora()
+calculadora.inicia()
