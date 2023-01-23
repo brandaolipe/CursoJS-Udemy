76648b331656c3011d3a9072a2db6836abbf3596
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-01-23 10:16:48 -0300

    Aula 62 - Revisão do básico em Arrays

---
+++ aula62/aula62.js
@@ -0,0 +1,33 @@
+
+// const nomes = ['Eduardo', 'Maria', 'Joana']
+// //const nomes = new Array('Eduardo', 'Maria', 'Joana')
+
+// //const novo = nomes // Valor por referência, mudou um dos dois mudou os dois
+// const novo = [...nomes]
+
+// //const removido = nomes.pop() // Remove do final
+// //const removido = nomes.shift() // Remove do começo, altera os indices
+// nomes.push('Gabriela') // Adiciona no final do array
+// nomes.unshift('Laura') // Adiciona no começo do array, altera os indices
+
+// //nomes[2] = 'João'
+// //delete nomes[2]
+
+// //console.log(nomes)
+// //console.log(novo)
+
+// console.log(nomes)
+
+// //                 0         1        2         3          4
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']
+// const novo = nomes.slice(1, -1)
+
+// console.log(novo)
+
+// const nome = 'Luiz Otávio Miranda'
+// const nomes = nome.split(' ') // Separa a string para um array
+// console.log(nomes)
+
+const nomes = ['Luiz', 'Otávio', 'Miranda']
+const nome = nomes.join(' ') // Junta o array em uma String
+console.log(nome)
