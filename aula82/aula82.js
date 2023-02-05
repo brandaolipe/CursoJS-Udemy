1d3c381e98e3ad39797bca663cd3ae8b198be48f
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2023-02-05 07:23:21 -0300

    Aula 82 - Herança de classes

---
+++ aula82/aula82.js
@@ -0,0 +1,53 @@
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligado = false;
+
+    }
+
+    ligar() {
+        if(this.ligado){
+            console.log(`${this.nome} já ligado.`);
+        }
+
+        this.ligado = true;
+    }
+
+    desligar() {
+        if(!this.ligado){
+            console.log(`${this.nome} já desligado.`);
+        }
+
+        this.ligado = false;
+    }
+}
+
+class Smartphone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        super(nome);
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+class Tablet extends DispositivoEletronico {
+    constructor(nome, temWifi) {
+        super(nome);
+        this.temWifi = temWifi;
+    }
+
+    ligar() {
+        console.log('Olha, você alterou o método ligar.');
+    }
+
+    falaOi() {
+        console.log('Oi');
+    }
+}
+
+
+const s1 = new Smartphone('Samsung', 'Preto', 'Galaxy S10');
+console.log(s1);
+
+const t1 = new Tablet('iPad', true);
+t1.ligar();
