e27943d64db1093015e874a92b51903cabcaa80e
brandaolipe <108373884+brandaolipe@users.noreply.github.com>
2024-02-21 12:22:13 -0300

    Aula 96 - gerador de senhas

---
+++ aula96/src/modules/formGeraSenha.js
@@ -0,0 +1,27 @@
+import geraSenha from './geradores';
+
+const senhaGerada = document.querySelector('.senha-gerada');
+const qtdCaracteres = document.querySelector('.qtd-caracteres');
+const maiusculas = document.querySelector('.chk-maiusculas');
+const minusculas = document.querySelector('.chk-minusculas');
+const numeros = document.querySelector('.chk-numeros');
+const simbolos = document.querySelector('.chk-simbolos');
+const gerarSenha = document.querySelector('.gerar-senha')
+
+export default () => {
+  gerarSenha.addEventListener('click', () => {
+    senhaGerada.innerText = gera();
+  });
+}
+
+function gera() {
+  const senha = geraSenha(
+    qtdCaracteres.value,
+    maiusculas.checked,
+    minusculas.checked,
+    numeros.checked,
+    simbolos.checked
+    );
+
+    return senha || 'Nada selecionado.';
+}
